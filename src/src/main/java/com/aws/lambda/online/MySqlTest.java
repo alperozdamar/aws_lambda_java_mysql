bd1d15970e5ee2db31f52d752dc7c940d2e967fc
AlperOzdamar <alper.ozdamar@gmail.com>
2019-09-09 12:08:14 -0700

    Initial Version
    Initial version

---
+++ src/src/main/java/com/aws/lambda/online/MySqlTest.java
@@ -0,0 +1,78 @@
+package com.aws.lambda.online;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.aws.lambda.online.data.RequestDetails;
+import com.aws.lambda.online.data.ResponseDetails;
+
+
+public class MySqlTest implements RequestHandler<RequestDetails, ResponseDetails>
+{
+	
+	public static LambdaLogger logger;
+   		
+	public ResponseDetails handleRequest(RequestDetails requestDetails, Context context) {
+		logger = context.getLogger();
+		
+		logger.log("Lambda function is invoked:"+requestDetails.getEmpID() + "," + requestDetails.getEmpName());
+		
+		// TODO Auto-generated method stub
+		ResponseDetails responseDetails = new ResponseDetails();
+				try {
+					insertDetails(requestDetails, responseDetails);
+				} catch (SQLException sqlException) {
+					responseDetails.setMessageID("999");
+					responseDetails.setMessageReason("Unable to Registor "+sqlException);
+				}
+				return responseDetails;
+	}
+
+	private void insertDetails(RequestDetails requestDetails, ResponseDetails responseDetails) throws SQLException {
+		Connection connection = getConnection();
+		Statement statement = connection.createStatement();
+		String query = getquery(requestDetails);
+		int responseCode = statement.executeUpdate(query);
+		if(1 == responseCode)
+		{
+			responseDetails.setMessageID(String.valueOf(responseCode));
+			responseDetails.setMessageReason("Successfully updated details");
+		}
+		
+	}
+
+	private String getquery(RequestDetails requestDetails) {
+		
+		String query = "INSERT INTO testdb.employee(empid, empname) VALUES (";
+		if (requestDetails != null) {
+			query = query.concat("'" + requestDetails.getEmpID() + "','" 
+					+ requestDetails.getEmpName() + "')");
+		}
+		System.out.println("the query is "+query);
+		return query;
+	}
+
+	private Connection getConnection() throws SQLException {
+        
+		logger.log("Waiting to GetConnection.....");
+		
+		// TODO Auto-generated method stub
+				String url = "jdbc:mysql://mysqltest.cqfxlfeycbgd.us-west-2.rds.amazonaws.com:3306";
+				String username = "root";
+				String password = "alper123";
+				
+				
+				
+				Connection conn = DriverManager.getConnection(url, username, password);
+				
+				logger.log("Connection established.....");
+				
+				return conn;
+	}
+
+}
---
+++ src/src/main/java/com/aws/lambda/online/data/RequestDetails.java
@@ -0,0 +1,23 @@
+package com.aws.lambda.online.data;
+
+public class RequestDetails {
+	private String empID;
+	private String empName;
+
+	public String getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+}
